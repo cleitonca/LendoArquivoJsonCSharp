3c6be57c166177d850887484a311feed9e7f9aa3
Cleiton Araújo <cltn.info@gmail.com>
2022-10-13 14:32:34 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using LendoArquivoJsonCSharp.Serialization;
+using System;
+using System.Runtime.Serialization.Json;
+using System.Text;
+
+namespace LendoArquivoJsonCSharp
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //var json = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"\pessoa.json");
+            var json = File.ReadAllText(@"D:\Estudo de Desenvolvimento\CSharp\TrabalhandoComArquivos\LendoArquivoJsonCSharp\pessoa.json");
+
+
+            //Utilizando o System.Runtime.Serialization 
+
+            var js = new DataContractJsonSerializer(typeof(List<Pessoa>));
+
+            var ms = new MemoryStream(Encoding.UTF8.GetBytes(json));
+
+            var pessoa = (List<Pessoa>)js.ReadObject(ms);
+
+
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("Nome:" + pessoa[i].Nome);
+                Console.WriteLine("Idade:" + pessoa[i].Idade);
+                Console.WriteLine("Empresa:" + pessoa[i].Empresa);
+
+                Console.WriteLine("----------------------");
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ Serialization/Pessoa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LendoArquivoJsonCSharp.Serialization
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+
+        public int Idade { get; set; }
+
+        public string Empresa { get; set; }
+
+        public List<Endereco> Endereco { get; set; }
+    }
+}
